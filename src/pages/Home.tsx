3e61e9490f15ca4e4d0c0fdd97fe02b5711e3fc6
Neamul01 <neamulhaque2002@gmail.com>
2024-10-06 23:39:19 +0600

    change some context

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,9 @@
-import { Link } from 'react-router-dom';
-import ToolInspection from './dashboard/ToolInspection';
+import Dashboard from './dashboard/Dashboard';
 
 const Home = () => {
   return (
     <div>
-      <h1>Home Page (Public)</h1>
-      <Link to="/login">Login</Link>
-      <br />
-      <Link to="/dashboard">Go to Dashboard (Private)</Link>
-
-      <ToolInspection />
+      <Dashboard />
     </div>
   );
 };
